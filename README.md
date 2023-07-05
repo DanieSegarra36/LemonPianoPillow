e8dbdd81f62b66541fa8dd582e83d2e54d2e6054
Daniel Segarra <danielsegarra36@gmail.com>
2023-07-05 16:25:01 -0400

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Lighthouse-Bubble-Hammer
---
+++ script.js
@@ -0,0 +1,34 @@
+function renderWindow(filePath) {
+  let element = document.querySelector("#renderWindow");
+  element.style.backgroundColor = filePath === "" ? "black" : "unset";
+  element.src = filePath;
+}
+
+const cohortsDropdown = document.getElementById("cohorts");
+
+cohortsDropdown.addEventListener("change", function () {
+  // clear preview/render window
+  renderWindow("");
+
+  const selectedValue = cohortsDropdown.value;
+  const selectedUL = document.getElementById(selectedValue);
+
+  // Hide all ul elements
+  const cohortFinalProjects = document.querySelectorAll(
+    ".cohort-final-projects"
+  );
+  cohortFinalProjects.forEach((list) => list.classList.add("hidden"));
+
+  // Show the selected ul element
+  if (selectedUL) {
+    selectedUL.classList.remove("hidden");
+  }
+});
+
+// Reset the dropdown select on page refresh
+window.addEventListener("load", function () {
+  renderWindow("");
+  // const selectedUL = document.getElementById("ITAG-2022-Cohort-4");
+  // selectedUL.classList.remove("hidden");
+  cohortsDropdown.selectedIndex = 0;
+});
